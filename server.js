763544f5f86172804c40be2d95b15482255d2383
Samantha Becker <Sam@MacBook-Pro.local>
2021-04-15 21:19:47 -0400

    Mongo setup

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+const session = require('express-session')
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: true,
+}))
+
+const mongoose = require('mongoose');
+mongoose.connect(
+    'mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PW}@cluster0.zpmbu.mongodb.net/whiteboard?retryWrites=true&w=majority',
+    {useNewUrlParser: true, useUnifiedTopology: true});
+
+// Configures CORS
+app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.header('Access-Control-Allow-Headers',
+        'Content-Type, X-Requested-With, Origin');
+    res.header('Access-Control-Allow-Methods',
+        'GET, POST, PUT, PATCH, DELETE, OPTIONS');
+    res.header("Access-Control-Allow-Credentials", "true");
+    next();
+});
+
+require('./controllers/users-controller')(app)
+
+app.listen(4000)
