59d107d9897527d45ac6fcfcc5e360dc6af10895
Samantha Becker <Sam@MacBook-Pro.local>
2021-04-15 22:35:05 -0400

    Edits

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 const express = require('express')
 const app = express()
+
 const bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
+
+app.get('/', (req, res) => {
+    res.send('Hello')
+})
 
 const session = require('express-session')
 app.use(session({
