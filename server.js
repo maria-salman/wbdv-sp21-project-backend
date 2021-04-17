44738b8359ceea28ac32def8b70e3625b1955b3f
Samantha Becker <Sam@MacBook-Pro.local>
2021-04-16 21:21:45 -0400

    React heroku url updated

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ mongoose.connect(
 
 // Configures CORS
 app.use(function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.header('Access-Control-Allow-Origin', 'http://wbdv-sp21-finalproject.herokuapp.com');
     res.header('Access-Control-Allow-Headers',
         'Content-Type, X-Requested-With, Origin');
     res.header('Access-Control-Allow-Methods',
