a1b63cdcef96e82d6d421d153e8313a055f918fe
Samantha Becker <Sam@MacBook-Pro.local>
2021-04-17 23:12:39 -0400

    Remote link

--- server.js
+++ server.js
@@ -17,23 +17,24 @@ app.use(session({
     saveUninitialized: true,
 }))
 
-// const db_link = 'mongodb+srv://' + process.env.DB_USER + ':' + process.env.DB_PASS + '@cluster0.rbkqs.mongodb.net/tripbook?retryWrites=true&w=majority'
-
-// const mongoose = require('mongoose');
-// mongoose.connect(
-//    db_link,
-//    {useNewUrlParser: true, useUnifiedTopology: true});
+const db_link = 'mongodb+srv://' + process.env.DB_USER + ':' + process.env.DB_PASS + '@cluster0.rbkqs.mongodb.net/tripbook?retryWrites=true&w=majority'
 
 const mongoose = require('mongoose');
 mongoose.connect(
-    'mongodb://localhost:27017/whiteboard-02',
-    {useNewUrlParser: true, useUnifiedTopology: true});
+   db_link,
+   {useNewUrlParser: true, useUnifiedTopology: true});
+
+// const mongoose = require('mongoose');
+// mongoose.connect(
+//     'mongodb://localhost:27017/whiteboard-02',
+//     {useNewUrlParser: true, useUnifiedTopology: true});
 
 // http://wbdv-sp21-finalproject.herokuapp.com
+// http://localhost:3000
 
 // Configures CORS
 app.use(function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.header('Access-Control-Allow-Origin', 'http://wbdv-sp21-finalproject.herokuapp.com');
     res.header('Access-Control-Allow-Headers',
         'Content-Type, X-Requested-With, Origin');
     res.header('Access-Control-Allow-Methods',
