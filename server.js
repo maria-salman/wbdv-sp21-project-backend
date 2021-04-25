ae0a999be39f859052691fbbe5664e8b765b48f0
Samantha Becker <Sam@MacBook-Pro.local>
2021-04-25 16:23:16 -0400

    Heroku

--- server.js
+++ server.js
@@ -17,22 +17,23 @@ app.use(session({
     saveUninitialized: true,
 }))
 
-const mongoose = require('mongoose');
-mongoose.connect(
-    'mongodb://localhost:27017/whiteboard-04',
-    {useNewUrlParser: true, useUnifiedTopology: true});
-// const db_link = 'mongodb+srv://' + process.env.DB_USER + ':' + process.env.DB_PASS + '@cluster0.rbkqs.mongodb.net/tripbook?retryWrites=true&w=majority'
-//
 // const mongoose = require('mongoose');
 // mongoose.connect(
-//     db_link,
+//     'mongodb://localhost:27017/whiteboard-04',
 //     {useNewUrlParser: true, useUnifiedTopology: true});
+
+const db_link = 'mongodb+srv://' + process.env.DB_USER + ':' + process.env.DB_PASS + '@cluster0.rbkqs.mongodb.net/tripbook?retryWrites=true&w=majority'
+
+const mongoose = require('mongoose');
+mongoose.connect(
+    db_link,
+    {useNewUrlParser: true, useUnifiedTopology: true});
 
 // http://wbdv-sp21-finalproject.herokuapp.com
 // http://localhost:3000
 
 app.use(function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.header('Access-Control-Allow-Origin', 'http://wbdv-sp21-finalproject.herokuapp.com');
     res.header('Access-Control-Allow-Headers',
         'Content-Type, X-Requested-With, Origin');
     res.header('Access-Control-Allow-Methods',
