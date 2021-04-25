66adf36ed5a3938b4c989665a47ac97c9f6930b6
Samantha Becker <Sam@MacBook-Pro.local>
2021-04-25 16:28:53 -0400

    Origin updated

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ mongoose.connect(
 // http://localhost:3000
 
 app.use(function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', 'http://wbdv-sp21-finalproject.herokuapp.com');
+    res.header('Access-Control-Allow-Origin', 'https://wbdv-sp21-finalproject.herokuapp.com');
     res.header('Access-Control-Allow-Headers',
         'Content-Type, X-Requested-With, Origin');
     res.header('Access-Control-Allow-Methods',
